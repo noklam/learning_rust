58fba6a649f05b33cb631da9e341543c224c2980
noklam <nok.lam.chan@quantumblack.com>
2022-04-27 23:04:34 +0100

    Refactor parse config logic

--- 03_grep/nokgrep/src/main.rs
+++ 03_grep/nokgrep/src/main.rs
@@ -1,16 +1,22 @@
 use std::env;
 use std::fs;
 
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
-
+    let (query, filename) = parse_config(&args);
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
     println!("Keywords: {}", query);
     println!("In file {}", filename);
     println!("Contents: {}", contents)
+
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
