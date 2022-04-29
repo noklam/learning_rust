9a9861d743b6176a6ef3c442bd5c2329b6082aa7
noklam <nok.lam.chan@quantumblack.com>
2022-04-29 16:15:53 +0100

    update now with error handling

--- 03_grep/nokgrep/src/main.rs
+++ 03_grep/nokgrep/src/main.rs
@@ -1,23 +1,28 @@
 use std::env;
 use std::fs;
-use std::fmt;
+use std::process;
 
 fn main() {
+    // --snip--
+
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-    println!("{}", &config);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
-
-
-    // --snip--
 }
 
 // --snip--
@@ -28,19 +33,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
-    }
-}
-
-impl fmt::Display for Config{
-    fn fmt (&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Query: ({}, {})", &self.query, &self.filename)
+        Ok(Config { query, filename })
     }
 }
