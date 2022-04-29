1e7145a17aed8e5cd942da4116ad3d2bba88abc2
noklam <nok.lam.chan@quantumblack.com>
2022-04-29 16:10:28 +0100

    Add Traits

--- 03_grep/nokgrep/src/main.rs
+++ 03_grep/nokgrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::fmt;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,11 +9,13 @@ fn main() {
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
+    println!("{}", &config);
 
     let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+
 
     // --snip--
 }
@@ -26,9 +29,18 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
         Config { query, filename }
+    }
+}
+
+impl fmt::Display for Config{
+    fn fmt (&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Query: ({}, {})", &self.query, &self.filename)
     }
 }
